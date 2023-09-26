4254844cdf149d4ca56c9a478d4559cf7a94266d
wfc <39686870+weifucheng1102@users.noreply.github.com>
2023-09-26 16:15:33 +0800

    Update SwiftLightPlugin.swift

--- packages/light/ios/Classes/SwiftLightPlugin.swift
+++ packages/light/ios/Classes/SwiftLightPlugin.swift
@@ -3,12 +3,30 @@ import UIKit
     
 public class SwiftLightPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "light", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "light.eventChannel", binaryMessenger: registrar.messenger())
     let instance = SwiftLightPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+    instance.cameraBrightnessHandler = CameraBrightnessHandler()        
+    eventChannel.setStreamHandler(StreamHandler(cameraBrightnessHandler: instance.cameraBrightnessHandler!))
+    }  }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     result("iOS " + UIDevice.current.systemVersion)
   }
+}
+class StreamHandler: NSObject, FlutterStreamHandler {
+    private var cameraBrightnessHandler: CameraBrightnessHandler
+
+    init(cameraBrightnessHandler: CameraBrightnessHandler) {
+        self.cameraBrightnessHandler = cameraBrightnessHandler
+    }
+
+    public func onListen(withArguments arguments: Any?, eventSink: @escaping FlutterEventSink) -> FlutterError? {
+        cameraBrightnessHandler.startCapturingBrightness(eventSink: eventSink)
+        return nil
+    }
+
+    public func onCancel(withArguments arguments: Any?) -> FlutterError? {
+        cameraBrightnessHandler.stopCapturingBrightness()
+        return nil
+    }
 }
